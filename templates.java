adc4106c77f6223f5175c1b809e4fe18c6500972
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2022-12-07 16:10:25 +0200

    Create templates.java

---
+++ templates.java
@@ -0,0 +1,9 @@
+//Generate Stack
+public static void main(String[] args){
+        Stack<Integer> s = new Stack<Integer>();
+        for(int i = -2; i < 5; i++)
+        {
+            s.push(i);
+        }
+        System.out.println(CallSomeFunction(x));
+}
