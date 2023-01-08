702abd45f7d4b5da28c1da5f94ba8ff3d7af39d6
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-01-08 20:28:32 +0200

    Delete templates.java

--- templates.java
+++
@@ -1,9 +0,0 @@
-//Generate Stack
-public static void main(String[] args){
-        Stack<Integer> s = new Stack<Integer>();
-        for(int i = 0; i < 5; i++)
-        {
-            s.push(i);
-        }
-        System.out.println(CallSomeFunction(x));
-}
